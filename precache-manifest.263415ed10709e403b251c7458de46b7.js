1061ada7df3db60eacc6e581b59424004beae778
Mohamed El-Ghoneimy <ghoneishy@hotmail.com>
2019-01-26 11:44:05 +0200

    Updates

---
+++ precache-manifest.263415ed10709e403b251c7458de46b7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/neighborhood-map-react/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "5512b3a016a48742cd79",
+    "url": "/neighborhood-map-react/static/js/runtime~main.5512b3a0.js"
+  },
+  {
+    "revision": "0e845a4665be4495e79e",
+    "url": "/neighborhood-map-react/static/js/main.0e845a46.chunk.js"
+  },
+  {
+    "revision": "fa92c112869d3657d47b",
+    "url": "/neighborhood-map-react/static/js/1.fa92c112.chunk.js"
+  },
+  {
+    "revision": "0e845a4665be4495e79e",
+    "url": "/neighborhood-map-react/static/css/main.64d40e04.chunk.css"
+  },
+  {
+    "revision": "82b2ffc4ed4c5ad0541e7a2e2275aa6e",
+    "url": "/neighborhood-map-react/index.html"
+  }
+];
